22b93c0f964a92e2c63886cc1eaa10ce74dda90f
franco-buccellato <francbuccellato@frba.utn.edu.ar>
2023-08-06 21:30:55 -0300

    Modificacion en Nosotros

--- src/componentes/SeccionQuienesSomos/SeccionQuienesSomos.js
+++ src/componentes/SeccionQuienesSomos/SeccionQuienesSomos.js
@@ -10,11 +10,15 @@ function SeccionQuienesSomos() {
                 <h2 className="display-6">¿Quienes Somos?</h2>
             </div>  
             <div className="section-nosotros-descripcion">
-                <img src={logoAmida}></img>
+                {/* <img src={logoAmida}></img> */}
                 <h6 className="display-6-configurado">
-                    Somos una empresa especializada en eventos y viajes corporativos. <br></br>
-                    Brindamos servicios personalizados y estratégicos, aportando mayor bienestar laboral y mejorando el sentido de pertenencia. <br></br>
-                    Nos involucramos en la transformación de las personas, creando resultados positivos y nutritivos que colaboran en una mayor integración y relacionamiento en las organizaciones.</h6>
+                    Somos Amida Group, una empresa especializada en eventos y viajes corporativos.<br></br><br></br>
+                    Brindamos servicios personalizados y estratégicos, aportando mayor bienestar laboral y mejorando el sentido de pertenencia.<br></br><br></br>
+                    Nos involucramos en la transformación de las personas, creando resultados positivos y nutritivos que colaboran en una mayor integración y relacionamiento en las organizaciones.<br></br><br></br>
+                    A lo largo de los años hemos desempeñado diversos roles dentro de la organización de eventos y viajes corporativos, adquiriendo experiencia en la planificación, coordinación y ejecución de diversos eventos y a su vez, brindando a nuestros clientes experiencias inolvidables.<br></br><br></br>
+                    Nuestra formación nos permite abordar desafíos con una perspectiva integral, enfocada en el crecimiento de nuestro negocio y en ofrecer un servicio diferenciado y excepcional a nuestros clientes.
+            
+                </h6>
             </div>
         </div>
     );
